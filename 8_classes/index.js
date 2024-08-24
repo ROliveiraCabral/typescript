74aef312fd5d1b1d636c3e1581b009d32a603131
ROCabral <rocabral.adm@gmail.com>
2024-08-24 15:43:17 -0300

    avançando nos estudos de classes com TS.

--- 8_classes/index.js
+++ 8_classes/index.js
@@ -14,3 +14,89 @@ class NewUser {
 }
 const joao = new NewUser("João", 8);
 console.log(joao);
+//3 - campo readonly
+class Car {
+    constructor(name) {
+        this.wheels = 4;
+        this.name = name;
+    }
+}
+const fusca = new Car("fusca");
+console.log(fusca);
+console.log(fusca.wheels);
+//4 - herança e super
+class Machine {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const trator = new Machine("trator");
+console.log(trator);
+class KillerMachine extends Machine {
+    constructor(name, guns) {
+        super(name);
+        this.guns = guns;
+    }
+}
+const destroyer = new KillerMachine("destroyer", 4);
+console.log(destroyer);
+//5 - métodos
+class Dwarf {
+    constructor(name) {
+        this.name = name;
+    }
+    greeting() {
+        console.log("Hey stranger!");
+    }
+}
+const jimmy = new Dwarf("Jimmy");
+console.log(jimmy);
+console.log(jimmy.name);
+jimmy.greeting();
+//6 - this
+class Truck {
+    constructor(model, hp) {
+        this.model = model;
+        this.hp = hp;
+    }
+    showDetails() {
+        console.log(`caminhão do modelo ${this.model}, que possui ${this.hp} cavalos de potência`);
+    }
+}
+const volvo = new Truck("Volvo", 400);
+const scania = new Truck("Scania", 500);
+volvo.showDetails();
+scania.showDetails();
+//7 - getter
+class Person {
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    get fullname() {
+        return console.log(this.name + " " + this.surname);
+    }
+}
+const patricia = new Person("Patrícia", "Ally");
+patricia.fullname;
+//8 - setter
+class Coords {
+    set fillx(x) {
+        if (x === 0) {
+            return;
+        }
+        this.x = x;
+        console.log("x inserido com sucesso");
+    }
+    set filly(y) {
+        if (y === 0) {
+            return;
+        }
+        this.y = y;
+        console.log("y inserido com sucesso");
+    }
+}
+const myCoords = new Coords();
+myCoords.fillx = 10;
+myCoords.filly = 5;
+console.log(myCoords);
--- 8_classes/index.ts
+++ 8_classes/index.ts
@@ -23,3 +23,130 @@ class NewUser {
 
 const joao = new NewUser("João", 8);
 console.log(joao);
+
+//3 - campo readonly
+class Car {
+  name;
+  readonly wheels = 4;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const fusca = new Car("fusca");
+
+console.log(fusca);
+console.log(fusca.wheels);
+
+//4 - herança e super
+class Machine {
+  name;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const trator = new Machine("trator");
+console.log(trator);
+
+class KillerMachine extends Machine {
+  guns;
+
+  constructor(name: string, guns: number) {
+    super(name);
+    this.guns = guns;
+  }
+}
+
+const destroyer = new KillerMachine("destroyer", 4);
+console.log(destroyer);
+
+//5 - métodos
+class Dwarf {
+  name;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  greeting() {
+    console.log("Hey stranger!");
+  }
+}
+
+const jimmy = new Dwarf("Jimmy");
+console.log(jimmy);
+console.log(jimmy.name);
+jimmy.greeting();
+
+//6 - this
+class Truck {
+  model;
+  hp;
+
+  constructor(model: string, hp: number) {
+    this.model = model;
+    this.hp = hp;
+  }
+
+  showDetails() {
+    console.log(
+      `caminhão do modelo ${this.model}, que possui ${this.hp} cavalos de potência`
+    );
+  }
+}
+
+const volvo = new Truck("Volvo", 400);
+const scania = new Truck("Scania", 500);
+volvo.showDetails();
+scania.showDetails();
+
+//7 - getter
+class Person {
+  name;
+  surname;
+
+  constructor(name: string, surname: string) {
+    this.name = name;
+    this.surname = surname;
+  }
+
+  get fullname() {
+    return console.log(this.name + " " + this.surname);
+  }
+}
+
+const patricia = new Person("Patrícia", "Ally");
+patricia.fullname;
+
+//8 - setter
+class Coords {
+  x!: number;
+  y!: number;
+
+  set fillx(x: number) {
+    if (x === 0) {
+      return;
+    }
+    this.x = x;
+
+    console.log("x inserido com sucesso");
+  }
+
+  set filly(y: number) {
+    if (y === 0) {
+      return;
+    }
+    this.y = y;
+
+    console.log("y inserido com sucesso");
+  }
+}
+
+const myCoords = new Coords();
+myCoords.fillx = 10;
+myCoords.filly = 5;
+
+console.log(myCoords);
