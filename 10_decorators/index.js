ed576dde150997f79650a4ecef578e2358615ed8
ROCabral <rocabral.adm@gmail.com>
2024-09-02 18:32:12 -0300

    concluindo estudos sobre decorators.

--- 10_decorators/index.js
+++ 10_decorators/index.js
@@ -90,3 +90,141 @@ __decorate([
 ], Machine.prototype, "showName", null);
 const trator = new Machine("Trator");
 console.log(trator.showName());
+// 5 - acessor decorator
+class Monster {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    get showName() {
+        return `O nome do monstro é: ${this.name}`;
+    }
+    get showAge() {
+        return `A idade do monstro é: ${this.age}`;
+    }
+}
+__decorate([
+    enumerable(true)
+], Monster.prototype, "showName", null);
+__decorate([
+    enumerable(true)
+], Monster.prototype, "showAge", null);
+const charmander = new Monster("charmander", 10);
+console.log(charmander);
+// 6 - property decorator
+//00001
+function formatNumber() {
+    return function (target, propertKey) {
+        let value;
+        const getter = function () {
+            return value;
+        };
+        const setter = function (newVal) {
+            value = String(newVal).padStart(5, "0");
+        };
+        Object.defineProperty(target, propertKey, {
+            get: getter,
+            set: setter,
+        });
+    };
+}
+class Id {
+    constructor(id) {
+        this.id = id;
+    }
+}
+__decorate([
+    formatNumber()
+], Id.prototype, "id", void 0);
+const newItem = new Id("1");
+console.log(newItem);
+console.log(newItem.id);
+// 7 - exemplo real com class decorator
+function createdDate(created) {
+    created.prototype.createdAt = new Date();
+}
+let Book = class Book {
+    constructor(id) {
+        this.id = id;
+    }
+};
+Book = __decorate([
+    createdDate
+], Book);
+let Pen = class Pen {
+    constructor(id) {
+        this.id = id;
+    }
+};
+Pen = __decorate([
+    createdDate
+], Pen);
+const newBook = new Book(12);
+const newPen = new Pen(55);
+console.log(newBook.createdAt);
+// 8 - exemplo real com method decorator
+function checkIfUserPosted() {
+    return function (target, key, descriptor) {
+        const childFunction = descriptor.value;
+        //console.log(childFunction);
+        descriptor.value = function (...args) {
+            if (args[1] === true) {
+                console.log("Usuário já postou!");
+                return null;
+            }
+            else {
+                return childFunction.apply(this, args);
+            }
+        };
+        return descriptor;
+    };
+}
+class Post {
+    constructor() {
+        this.alreadyPosted = false;
+    }
+    post(content, alreadyPosted) {
+        this.alreadyPosted = true;
+        console.log(`post do usuário: ${content}`);
+    }
+}
+__decorate([
+    checkIfUserPosted()
+], Post.prototype, "post", null);
+const newPost = new Post();
+newPost.post("Meu primeiro post!", newPost.alreadyPosted);
+newPost.post("Meu segundo post!", newPost.alreadyPosted);
+newPost.post("Meu Terceiro post!", newPost.alreadyPosted);
+// 9 - exemplo real com property decorator
+function max(limit) {
+    return function (target, propertKey) {
+        let value;
+        const getter = function () {
+            return value;
+        };
+        const setter = function (newVal) {
+            if (newVal.length > limit) {
+                console.log(`O valor deve ter no máximo ${limit} dígitos`);
+                return;
+            }
+            else {
+                value = newVal;
+            }
+        };
+        Object.defineProperty(target, propertKey, {
+            get: getter,
+            set: setter,
+        });
+    };
+}
+class Admin {
+    constructor(username) {
+        this.username = username;
+    }
+}
+__decorate([
+    max(10)
+], Admin.prototype, "username", void 0);
+const pedro = new Admin("pedroAdmin1234");
+const lee = new Admin("lee");
+console.log(lee);
--- 10_decorators/index.ts
+++ 10_decorators/index.ts
@@ -114,3 +114,165 @@ class Machine {
 
 const trator = new Machine("Trator");
 console.log(trator.showName());
+
+// 5 - acessor decorator
+class Monster {
+  name?;
+  age?;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @enumerable(true)
+  get showName() {
+    return `O nome do monstro é: ${this.name}`;
+  }
+
+  @enumerable(true)
+  get showAge() {
+    return `A idade do monstro é: ${this.age}`;
+  }
+}
+
+const charmander = new Monster("charmander", 10);
+console.log(charmander);
+
+// 6 - property decorator
+//00001
+function formatNumber() {
+  return function (target: Object, propertKey: string) {
+    let value: string;
+
+    const getter = function () {
+      return value;
+    };
+
+    const setter = function (newVal: string) {
+      value = String(newVal).padStart(5, "0");
+    };
+
+    Object.defineProperty(target, propertKey, {
+      get: getter,
+      set: setter,
+    });
+  };
+}
+
+class Id {
+  @formatNumber()
+  id;
+
+  constructor(id: string) {
+    this.id = id;
+  }
+}
+
+const newItem = new Id("1");
+console.log(newItem);
+console.log(newItem.id);
+
+// 7 - exemplo real com class decorator
+function createdDate(created: Function) {
+  created.prototype.createdAt = new Date();
+}
+
+@createdDate
+class Book {
+  id;
+  createdAt?: Date;
+
+  constructor(id: number) {
+    this.id = id;
+  }
+}
+
+@createdDate
+class Pen {
+  id;
+  constructor(id: number) {
+    this.id = id;
+  }
+}
+
+const newBook = new Book(12);
+const newPen = new Pen(55);
+
+console.log(newBook.createdAt);
+
+// 8 - exemplo real com method decorator
+function checkIfUserPosted() {
+  return function (
+    target: Object,
+    key: string | symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    const childFunction = descriptor.value;
+    //console.log(childFunction);
+    descriptor.value = function (...args: any[]) {
+      if (args[1] === true) {
+        console.log("Usuário já postou!");
+        return null;
+      } else {
+        return childFunction.apply(this, args);
+      }
+    };
+    return descriptor;
+  };
+}
+class Post {
+  alreadyPosted = false;
+
+  @checkIfUserPosted()
+  post(content: string, alreadyPosted: boolean) {
+    this.alreadyPosted = true;
+    console.log(`post do usuário: ${content}`);
+  }
+}
+
+const newPost = new Post();
+
+newPost.post("Meu primeiro post!", newPost.alreadyPosted);
+
+newPost.post("Meu segundo post!", newPost.alreadyPosted);
+
+newPost.post("Meu Terceiro post!", newPost.alreadyPosted);
+
+// 9 - exemplo real com property decorator
+function max(limit: number) {
+  return function (target: Object, propertKey: string) {
+    let value: string;
+
+    const getter = function () {
+      return value;
+    };
+
+    const setter = function (newVal: string) {
+      if (newVal.length > limit) {
+        console.log(`O valor deve ter no máximo ${limit} dígitos`);
+        return;
+      } else {
+        value = newVal;
+      }
+    };
+
+    Object.defineProperty(target, propertKey, {
+      get: getter,
+      set: setter,
+    });
+  };
+}
+
+class Admin {
+  @max(10)
+  username;
+
+  constructor(username: string) {
+    this.username = username;
+  }
+}
+
+const pedro = new Admin("pedroAdmin1234");
+const lee = new Admin("lee");
+console.log(lee);
